7d6d3fe28319679befe1f33c55c372351ce794ca
Patryk Krezel <43613117+Pat5657@users.noreply.github.com>
2022-01-21 00:44:44 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/database";
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAkqih8CUzxCkrU-wTpO14pe93K2J1lzlc",
-  authDomain: "padpad-svelte.firebaseapp.com",
-  databaseURL: "https://padpad-svelte.firebaseio.com",
-  projectId: "padpad-svelte",
-  storageBucket: "padpad-svelte.appspot.com",
-  messagingSenderId: "538031802538",
-  appId: "1:538031802538:web:874d5965ab1b4acfe268d3",
-  measurementId: "G-057P96DG8M"
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DB_URL,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID
 };
 
 // Initialize Firebase
